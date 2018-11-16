7bba1cf0c5e34715d51142d185d9c9c7070447fb
Paris Mavromoustakos <parismav87@gmail.com>
2018-11-16 23:19:32 +0100

    ajfhdv,nbvs,

--- lolbot/script.js
+++ lolbot/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	
 
 	$("#button-search").on("click", function(evt){
+		evt.preventDefault();
 		$("#page1").addClass("invisible").removeClass("visible");
 		$("#page2").addClass("visible").removeClass("invisible");
 	})
