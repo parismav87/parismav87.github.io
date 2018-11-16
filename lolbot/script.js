98316c9c653dc9c19d3f7638d8c26d73460e8845
parismav87 <parismav87@gmail.com>
2018-11-16 15:09:09 +0100

    lelbot first try

---
+++ lolbot/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	
+
+	$("#button-search").on("click", function(evt){
+		$("#page1").addClass("invisible").removeClass("visible");
+		$("#page2").addClass("visible").removeClass("invisible");
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
