23576ac62b65c472df00c2fc5537b68de0e5be3f
Paris Mavromoustakos <parismav87@gmail.com>
2020-04-09 01:32:44 +0200

    xxv

--- bourlo/script.js
+++ bourlo/script.js
@@ -354,7 +354,7 @@ function toggleBought(data){
 
 $( document ).ready(function() {
   console.log( "ready!" );
-  socket = io.connect("https://1b34ffc6.ngrok.io")
+  socket = io.connect("https://a562b535.ngrok.io")
 
 
   board = []
