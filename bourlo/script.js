50ea46ecc3f816f56259ad9cbc1fed675fc41ffe
Paris Mavromoustakos <parismav87@gmail.com>
2020-04-12 02:24:16 +0200

    ascmvn
    dew

--- bourlo/script.js
+++ bourlo/script.js
@@ -387,7 +387,7 @@ function toggleBought(data){
 
 $( document ).ready(function() {
   console.log( "ready!" );
-  socket = io.connect("https://4e2d5b31.ngrok.io")
+  socket = io.connect("https://f2db2353.ngrok.io")
 
 
   board = []
